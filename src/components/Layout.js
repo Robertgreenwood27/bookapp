ab92c3d59497ae8b2eefd5f507bb0beabd98b5fa
RobertGreenwood27@gmail.com <RobertGreenwood27@gmail.com>
2024-09-23 05:54:07 -0600

    09/23/2024-5:53am

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,27 +1,26 @@
-// src/components/Layout.js
-
 import React from 'react';
-
-// src/components/Layout.js
+import Image from 'next/image';
 
 export default function Layout({ children, backgroundImage }) {
-    return (
-      <div className="relative min-h-screen flex flex-col bg-background text-foreground">
-        {/* Background Image and Overlay */}
-        <div className="absolute inset-0">
-          <img
-            src={backgroundImage}
-            alt="Background"
-            className="w-full h-full object-cover object-center"
-          />
-          <div className="absolute inset-0 bg-black opacity-60"></div>
-        </div>
-  
-        {/* Content */}
-        <div className="relative z-10 flex flex-col flex-1">
-          {children}
-        </div>
+  return (
+    <div className="relative min-h-screen flex flex-col bg-background text-foreground">
+      {/* Background Image and Overlay */}
+      <div className="absolute inset-0">
+        <Image
+          src={backgroundImage}
+          alt="Background"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+          priority
+        />
+        <div className="absolute inset-0 bg-black opacity-60"></div>
       </div>
-    );
-  }
-  
+
+      {/* Content */}
+      <div className="relative z-10 flex flex-col flex-1">
+        {children}
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 // pages/index.js
+import Layout from '../components/layout'
 import TOC from '../components/toc';
 import fs from 'fs';
 import path from 'path';
 
 export async function getStaticProps() {
-  const contentDir = path.join(process.cwd(), 'content'); // Updated path
+  const contentDir = path.join(process.cwd(), 'content');
   const files = fs.readdirSync(contentDir);
   const chapters = files.map((filename) => filename.replace('.md', ''));
 
@@ -18,12 +19,10 @@ export async function getStaticProps() {
 export default function Home({ chapters }) {
   return (
     <Layout backgroundImage="/images/home-bg.jpg">
-    <div>
-      <h1>Throne of Shadows</h1> 
-      <TOC chapters={chapters} />
-      
-
-    </div>
+      <div>
+        <h1>Throne of Shadows</h1> 
+        <TOC chapters={chapters} />
+      </div>
     </Layout>
   );
 }
